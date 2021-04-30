57d3265c5526e2f427b2aac040f0a6d9b207e8c4
Rekha-15 <rekhapatil.1509@gmail.com>
2021-04-30 15:56:00 -0400

    add:EmployeeWageComputation

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,2 +1,11 @@
 #!/bin/bash -x
 echo " Welcome to Employee Wage Computation "
+ispresent=1
+randomCheck=$(( RANDOM%2 ))
+if [ $ispresent -eq $randomCheck ]
+then
+echo " Employee is present "
+else
+echo " Employee is absent "
+fi
+
