c4edd63109a27ec2eb4b2a185a3164b7da6369d1
Rekha-15 <rekhapatil.1509@gmail.com>
2021-05-01 13:24:15 -0400

    add: EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -1,11 +1,16 @@
 #!/bin/bash -x
 echo " Welcome to Employee Wage Computation "
 ispresent=1
+isFulltime=2
+empRatePerHour=20
+emphrs=8
 randomCheck=$(( RANDOM%2 ))
 if [ $ispresent -eq $randomCheck ]
 then
 echo " Employee is present "
+salary=$(($emphrs*$empRatePerHour))
+echo "$salary"
 else
-echo " Employee is absent "
+	echo " Employee is absent "
 fi
 
