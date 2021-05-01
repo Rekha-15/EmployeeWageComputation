b41015002059e68d4875ea9e9c60539de2f96885
Rekha-15 <rekhapatil.1509@gmail.com>
2021-05-01 16:04:19 -0400

    add: EmployeeWageComputation.sh

--- EmployeeWageComputation.sh
+++ EmployeeWageComputation.sh
@@ -16,23 +16,35 @@ function attendance()
 
 attendance
 
-isPartTime=1;
-isFullTime=2;
-empRatePerHr=20;
-randomCheck=$((RANDOM%3))
 
 
-case $randomCheck in
-        $isFullTime)
-	            empHrs=8;
-                       ;;
-        $isPartTime)
-                    empHrs=4;
-                       ;;
-               *)
-                    empHrs=0;
-esac
+IS_PART_TIME=1;
+IS_FULL_TIME=2;
+MAX_HRS_IN_MONTH=100;
+EMP_RATE_PER_HR=20;
+NUM_WORKING_DAYS=20;
 
-salary=$(($empHrs * $empRatePerHr));
+totalEmpHr=0;
+totalWorkingDays=0;
 
+while [[ $totalEmpHr -lt $MAX_HRS_IN_MONTH &&
+         $totalWorkingDays -lt $NUM_WORKING_DAYS ]]
+do
+      ((totalWorkingDays++))
+       empCheck=$((RANDOM%3));
+       case $empCheck in
+            $IS_FULL_TIME)
+                        empHrs=8
+                            ;;
+            $IS_PART_TIME)
+                        empHrs=4
+                             ;;
+            *)
+                         empHrs=0
+                             ;;
+        esac
+        totalEmpHr=$(($totalEmpHr+$empHrs))
+done
+
+totalSalary=$(($totalEmpHr*$EMP_RATE_PER_HR));
 
